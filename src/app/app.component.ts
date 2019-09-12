ba84b6107e7d4eaa69029db51e1514bf351171ba
jaho1986 <jaho-corp@hotmail.com>
2019-09-11 23:40:09 -0500

    Ahora tanto el menú, como la pantalla principal tienen diseños 100% responsivos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    $(document).ready(function () {
+      $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+      });
+  });
+  }
 }
